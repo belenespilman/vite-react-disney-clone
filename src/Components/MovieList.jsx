da82407335c3b99d8161a11156f76cfb1a6e6000
belenespilman <belen.espilman@gmail.com>
2024-01-22 18:28:03 +0100

    Movies by Genres Added

---
+++ src/Components/MovieList.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useRef, useEffect } from 'react'
+import GlobalApi from '../Services/GlobalApi'
+import MovieCard from './MovieCard'
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+import HrMovieCard from './HrMovieCard'
+
+
+
+
+function MovieList({genreId, index_}) {
+    const [movieList, setMovieList]= useState([])
+    const elementRef=useRef(null);
+    useEffect(()=>{
+        getMovieByGenreId();
+    }, [])
+
+    const getMovieByGenreId = async ()=> {
+        const res = await GlobalApi.getMovieByGenreId(genreId)
+        setMovieList(res.data.results)
+
+
+    }
+    const slideRight=(element)=>{
+        element.scrollLeft+=500;
+    }
+    const slideLeft=(element)=>{
+        element.scrollLeft-=500;
+    }
+
+  return (
+
+    
+    <div className='relative'>
+        <IoIosArrowBack onClick={()=>slideLeft(elementRef.current)} 
+    className={`text-[50px] text-white
+      p-2 z-10 cursor-pointer 
+       hidden md:block absolute
+       ${index_%3==0?'mt-[80px]':'mt-[150px]'} `}/>
+
+    <div ref={elementRef} className='flex overflow-x-auto gap-8
+scrollbar-none cursor-pointer w-screen scroll-smooth pt-4 px-3 pb-4'>
+   {movieList.map((item,index)=>(
+        <>
+            {index_ %3 == 0 ? <HrMovieCard movie={item}/> : <MovieCard movie={item} />}
+        </> 
+   ))}
+
+    </div>
+        <IoIosArrowForward onClick={()=>slideRight(elementRef.current)}
+      className={`text-[50px] text-white hidden md:block
+      p-2 cursor-pointer z-10 top-0
+       absolute right-0 
+       ${index_%3==0?'mt-[80px]':'mt-[150px]'}`}/> 
+    </div>
+  )
+}
+
+export default MovieList;
--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -3,10 +3,18 @@ import axios from "axios";
 const movieBaseUrl = "https://api.themoviedb.org/3"
 const api_key= "2982d6831cef8c918d2af60ec9e8afab" 
 
+const movieByGenreBaseURL= "https://api.themoviedb.org/3/discover/movie?api_key=2982d6831cef8c918d2af60ec9e8afab";
 
 //--url 'https://api.themoviedb.org/3/trending/\
 
 const getTrendingVideos=axios.get(`${movieBaseUrl}/trending/all/day?api_key=${api_key}`);
+    
+
+const getMovieByGenreId = async (id)=> {
+    const url = movieByGenreBaseURL + "&with_genres=" + id
+    const response = await axios.get(url)
+    return response
+}
 
 
-export default{ getTrendingVideos }
+export default{ getTrendingVideos, getMovieByGenreId }
