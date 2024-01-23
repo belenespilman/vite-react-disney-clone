355e1b1b687fa63726ca9a85b704281caa642c84
belenespilman <belen.espilman@gmail.com>
2024-01-23 11:17:41 +0100

    Fix overflow

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -4,9 +4,6 @@ import GlobalApi from '../Services/GlobalApi'
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 
-
-
-
 const IMAGE_BASE_URL= "https://image.tmdb.org/t/p/original"
 
 const screenWidth= window.innerWidth;
@@ -42,7 +39,7 @@ function Slider() {
         <IoIosArrowBack className='hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer' onClick={()=>{sliderLeft(elementRef.current)}}/>
         <IoIosArrowForward className='hidden md:block  text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0' onClick={()=>{sliderRight(elementRef.current)}}/>
         <div className='flex overflow-x-auto w-screen px-16 py-4 scrollbar-none scroll-smooth' ref={elementRef}>
-            {movieList.map((item, index)=>{
+            {movieList.map((item)=>{
                 return <img src={`${IMAGE_BASE_URL}${item.backdrop_path}`} className="min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md cursor-pointer hover:border-[4px] border-gray-400 transition-all duration-100 ease-in-out" alt={item.title || item.name} />
             })}
 
